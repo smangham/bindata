e99e5ea9c640d1c7f312f517c4118c5df6234354
Sam Mangham <mangham@gmail.com>
2016-11-30 20:07:14 +0000

    Latest

--- bindata.py
+++ bindata.py
@@ -7,198 +7,206 @@ This is a temporary script file.
 import sqlite3
 import numpy as np
 import astropy as ap
-from astropy.table import Table
 import time
+import sys
+import matplotlib.pyplot as plt
 
 class Query(object):
     def __init__(self, dbc, spectrum):
         if spectrum < 0:
-            print "Error: {} is not a valid observer".format(spectrum)
+            print("Error: {} is not a valid observer".format(spectrum))
             self.query = "ERROR"
         else:
-            self.spectrum = spectrum
             self.dbc = dbc
-            self.as_column_names = as_get_column_names(dbc)
-            self.s_query = "SELECT Wavelength, Delay, Weight FROM Photons WHERE Spectrum == {}".format(self.spectrum)
+            # PRAGMA returns entries like 'key|name|format|flag1|flag2|flag3'
+            self.column_names = [ entry[1] for entry in  dbc.execute("PRAGMA table_info(Photons)").fetchall()]
+            self.query = "SELECT Wavelength, Delay, Weight FROM Photons WHERE Spectrum == {}".format(spectrum)
+            self._equal = dict()
+            self._not_equal = dict()
+            self._in_range = dict()
+            self._not_in_range = dict()
+            self._in_list = dict()
+            self._not_in_list = dict()            
+            self._equal["Spectrum"] = spectrum
 
     def clear(self):
             self.query = "SELECT Wavelength, Delay, Weight FROM Photons WHERE Spectrum == {}".format(self.spectrum)
+            spectrum = self._equal["Spectrum"]            
+            self._equal.clear()
+            self._not_equal.clear()
+            self._in_range.clear()
+            self._not_in_range.clear()
+            self._in_list.clear()
+            self._not_in_list.clear()
+            self._equal["Spectrum"] = spectrum            
             return self
 
-    def equal(self, s_column, i_val):
-        if s_column not in self.as_column_names:
-            print "Error: Column '{}' does not exist in file.".format(s_column) 
+    def equal(self, column, value):
+        if column not in self.column_names:
+            print("Error: Column '{}' does not exist in file.".format(column))
         else:   
-            self.s_query = self.s_query + " AND {} == {}".format(s_column, i_val)
+            self._equal[column] = value
+            self.query = self.query + " AND {} == {}".format(column, value)
             return self
-    def not_equal(self, s_column, i_val):
-        if s_column not in self.as_column_names:
-            print "Error: Column '{}' does not exist in file.".format(s_column) 
+    def not_equal(self, column, value):
+        if column not in self.column_names:
+            print("Error: Column '{}' does not exist in file.".format(column)) 
         else:   
-            self.s_query = self.s_query + " AND {} != {}".format(s_column, i_val)
+            self._not_equal[column] = value
+            self.query = self.query + " AND {} != {}".format(column, value)
             return self
 
-    def in_range(self, s_column, r_val_min, r_val_max):
-        if s_column not in self.as_column_names:
-            print "Error: Column '{}' does not exist in file.".format(s_column) 
+    def in_range(self, column, value_min, value_max):
+        if column not in self.column_names:
+            print("Error: Column '{}' does not exist in file.".format(column) )
         else:  
-            self.s_query = self.s_query + " AND {} BETWEEN {} AND {}".format(s_column, r_val_min, r_val_max)
+            self._in_range[column] = [value_min, value_max]
+            self.query = self.query + " AND {} BETWEEN {} AND {}".format(column, value_min, value_max)
             return self
-    def not_in_range(self, s_column, r_val_min, r_val_max):
-        if s_column not in self.as_column_names:
-            print "Error: Column '{}' does not exist in file.".format(s_column) 
+    def not_in_range(self, column, value_min, value_max):
+        if column not in self.acolumn_names:
+            print("Error: Column '{}' does not exist in file.".format(column))
         else:  
-            self.s_query = self.s_query + " AND {} NOT BETWEEN {} AND {}".format(s_column, r_val_min, r_val_max)
+            self._not_in_range[column] = [value_min, value_max]
+            self.query = self.query + " AND {} NOT BETWEEN {} AND {}".format(column, value_min, value_max)
             return self
 
-    def in_list(self, s_column, ar_val):
-        if s_column not in self.as_column_names:
-            print "Error: Column '{}' does not exist in file.".format(s_column) 
+    def in_list(self, column, values):
+        if column not in self.column_names:
+            print("Error: Column '{}' does not exist in file.".format(column))
         else:  
-            self.s_query = self.s_query + " AND {} IN ({})".format(s_column,", ".join(map(str, ar_val)))
+            self._in_list[column] = values
+            self.query = self.query + " AND {} IN ({})".format(column,", ".join(map(str, values)))
             return self
-    def not_in_list(self, s_column, ar_val):
-        if s_column not in self.as_column_names:
-            print "Error: Column '{}' does not exist in file.".format(s_column) 
+    def not_in_list(self, column, values):
+        if column not in self.column_names:
+            print("Error: Column '{}' does not exist in file.".format(column))
         else:  
-            self.s_query = self.s_query + " AND {} NOT IN ({})".format(s_column,", ".join(map(str, ar_val)))
+            self._not_in_list[column] = values
+            self.query = self.query + " AND {} NOT IN ({})".format(column,", ".join(map(str, values)))
             return self
 
     def run(self):
-        return dbc.execute(self.s_query).fetchall()
+        return dbc.execute(self.query).fetchall()
 
-def as_get_column_names(dbc):
-    # PRAGMA returns entries like 'key|name|format|flag1|flag2|flag3'
-    return [ entry[1] for entry in  dbc.execute("PRAGMA table_info(Photons)").fetchall()]
-    
-s_root = "ngc5548_1e00_obs_2"
+
+class TransferFunction(Query):    
+    def __init__(self,dbc, dimensions, spectrum):
+        super(TransferFunction,self).__init__(dbc, spectrum)
+        self._dimensions = dimensions
+        self._flux = np.zeros(shape=dimensions, dtype=np.float64)
+        self._count = np.zeros(shape=dimensions, dtype=np.intc)
+        
+    def run(self):
+        data = np.asarray(super(TransferFunction,self).run())
+        # Data returned as Wavelength, Delay, Weight
+        if "Wavelength" in self._in_range:
+            range_wave = self._in_range["Wavelength"]
+        else:
+            print("Error: No wavelength range defined, autodetecting...")
+            range_wave = [np.amin(data[:,0]), np.amax(data[:,0])]                
+        
+        if "Delay" in self._in_range:
+            range_delay = self._in_range["Delay"]
+        else:
+            range_delay = [0,np.percentile(data[:,1],99)]
+            print("Error: No delay range defined, autodetecting to {:.0f}:{:.2f}".format(*range_delay))
+                        
+        self._bins_wave  = np.linspace(range_wave[0], range_wave[1],
+                                       self._dimensions[0]+1, endpoint=True, dtype=np.float64)
+        self._bins_delay = np.linspace(range_delay[0], range_delay[1],
+                                       self._dimensions[1]+1, endpoint=True, dtype=np.float64)
+
+        self._flux, junk, junk = np.histogram2d(data[:,1], data[:,0], weights=data[:,2], 
+                       bins=[self._bins_delay, self._bins_wave], normed=True)                    
+        self._count, junk, junk = np.histogram2d(data[:,0], data[:,1], 
+                       bins=[self._bins_delay, self._bins_wave], normed=True)
+        self._flux_min = np.amin(self._flux[np.nonzero(self._flux)])
+        self._flux[self._flux == 0] = 1e-35
+        return self
+        
+    def flux(self, wave, delay):
+        return(self._flux[np.searchsorted(self._bins_delay, delay),
+                          np.searchsorted(self._bins_wave, wave)])
+    def count(self, wave, delay):
+        return(self._count[np.searchsorted(self._bins_delay, delay),
+                           np.searchsorted(self._bins_wave, wave)])
+
+    def plot(self, range=4):
+        cb_max = np.log10(np.amax(self._flux))
+        cb_min = np.log10(np.amax(self._flux))-range
+        print(cb_min, cb_max)
+        
+        fig = plt.figure()
+        axis = fig.add_subplot(111)
+        tf = axis.pcolor(self._bins_wave, self._bins_delay, np.log10(self._flux),
+                         vmin=cb_min, cmap="afmhot_r")
+        axis.set_ylim(top=self._bins_delay[-1])
+        axis.set_aspect('auto')
+        cbar = plt.colorbar(tf, orientation="vertical")
+        fig.show()
+
+s_root = "test"
 s_file_db = s_root+".db"
 s_file_dd = s_root+".delay_dump"
 
 q_table_exists = "SELECT name FROM sqlite_master WHERE type='table' AND name='Photons'"
 q_table_add = """CREATE TABLE Photons(Key INT, Wavelength DOUBLE, Weight DOUBLE, X DOUBLE, Y DOUBLE, Z DOUBLE, 
               ContinuumScatters INT, ResonantScatters INT, Delay DOUBLE, Spectrum INT, Origin INT, Resonance INT)"""
-q_photon_add = "INSERT INTO Photons VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+q_photon_add = "INSERT INTO Photons VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 
 as_names = ["Frequency", "Wavelength", "Weight", "X", "Y", "Z", "Scatters", "Resonant Scatters", "Delay", "Extracted", "Spectrum", "Origin", "Resonance"]
-t_photons = Table.read(s_file_dd, format="ascii", names=as_names)
-t_photons.remove_columns(["Extracted", "Frequency"])
-t_photons["Scatters"] = t_photons["Scatters"] - t_photons["Resonant Scatters"]
-t_photons.rename_column('Scatters', "Continuum Scatters")
 
 ### TRY OPENING THE DATABASE ###
 db = None
 try:
     db = sqlite3.connect(s_file_db)
-except sqlite3.Error, e:
-    print "Error {}: ".format(e.args[0])
+except sqlite3.Error as e:
+    print("Error {}: ".format(e.args[0]))
     sys.exit(1)
 
 ### DOES IT ALREADY EXIST? ###
 dbc = db.cursor()
-if dbc.execute(q_table_exists).fetchone():
-    # If so, we go with what we've found.
-    print "Found existing filled photon database '{}'".format(s_file_db)
-else:
-    # If not, we populate from the delay dump file. This bit is legacy!
-    print "No existing filled photon database, reading from file '{}'".format(s_file_dd)
-    dbc = db.cursor()
-    dbc.execute("DROP TABLE IF EXISTS Photons")
-    dbc.execute(q_table_add)
 
-    i_key = 0
-    for line in t_photons:
-        dbc.execute(q_photon_add, line.insert(i_key,0))
-        key=key+1
-
-    db.commit()
 
 start = time.clock()
-Query(dbc, 1).equal("Resonance",416).in_range("Wavelength", 1475, 1625).run()
-elapsed = time.clock() - start
-print "Seconds ",elapsed,"Minutes",elapsed/60,"Hours",elapsed/360
-db.close()
+if dbc.execute(q_table_exists).fetchone():
+    # If so, we go with what we've found.
+    print("Found existing filled photon database '{}'".format(s_file_db))
+else:
+    # If not, we populate from the delay dump file. This bit is legacy!
+    print("No existing filled photon database, reading from file '{}'".format(s_file_dd))
+    dbc = db.cursor()
+    dbc.execute(q_table_add)
 
-
-
-# class Photon(object):
-#     def __init__(self, vals):
-#         print("INIT PHOTON: ",vals)
-#         self.freq = vals[1]       
-#         self.position = (vals[3], vals[4], vals[5])
-#         self.time = vals[6]
-#         self.weight = vals[2]
-#         self.origin = vals[8]
-#         self.observer = vals[7]
-#         self.last_scattered_line = vals[9]
-#         self.scatters_resonant = vals[4]
-#         self.scatters_continuum = vals[5] - vals[4]
-
-# class Input(object):
-#     def InputPhotons(filename):    
-#         photon_list = list()
-#         file = open(filename, 'r')
-#         for line in file:
-#             print(line)
-#             if(line.startswith('#')): 
-#                 continue
-#             vals = line.split()
-#             print("READ PHOTON: ",vals)
-#             photon_list.append(Photon(np.asarray(vals,float)))  
-#         return(photon_list)
-
-#     Photons = staticmethod(InputPhotons)
-
-
-# class TransferFunction(object):    
-#     def __init__(self,dimensions, range_freq, range_time, observer, 
-#                  last_scattered_lines=None, photon_origins=None, 
-#                  scatters_resonant=None, scatters_continuum=None):
-#         self._bins_freq = np.linspace(range_freq[0], range_freq[1],
-#                                       dimensions[0]+1, endpoint=True, dtype=np.float64)
-#         self._bins_time = np.linspace(range_time[0], range_time[1],
-#                                  dimensions[1]+1, endpoint=True, dtype=np.float64)
-#         self._bins_flux = np.zeroes(shape=dimensions, dtype=np.float64)
-#         self._bins_count = np.zeroes(shape=dimensions, type=np.intc)
-#         self._last_scattered_lines = last_scattered_lines
-#         self._scatters_resonant = scatters_resonant
-#         self._scatters_continuum = scatters_continuum
-#         self._photon_origins = photon_origins
-#         self._observer = observer        
+    key = 0
+    delay_dump = open(s_file_dd, 'r')
+    for line in delay_dump:
+        if line.startswith('#'): 
+            continue
+        values = line.split()
+        if len(values) < 12:
+            continue
         
-#     def addPhoton(self, freq, time, weight, origin, observer,
-#                   last_scattered_line, scatters_resonant, scatters_continuum):
-#         if self._observer != observer:
-#             pass
-#         elif not self._bins_time[0] < time < self._bins_time[-1]: 
-#             pass
-#         elif not self._bins_freq[0] < freq < self._bins_freq[-1]:
-#             pass
-#         elif self._photon_origins != None and \
-#             not origin in self._photon_origins:
-#             pass
-#         elif self._scatters_resonant != None and \
-#             not scatters_resonant in self._scatters_resonant:
-#             pass
-#         elif self._scatters_continuum != None and \
-#             not scatters_continuum in self._scatters_continuum:
-#             pass
-#         elif self._last_scattered_lines != None and \
-#             not last_scattered_line in self._last_scattered_lines:
-#             pass
-#         else:
-#             i_freq = np.searchsorted(self._bins_freq, freq)
-#             i_time = np.searchsorted(self._bins_time, time)
-#             self._bins_flux[i_freq, i_time] = self._bins_flux[i_freq, i_time] + weight
-#             self._bins_count[i_freq, i_time] = self._bins_count[i_freq, i_time] + 1
-                
-#     def flux(self, freq, time):
-#         return(self._bins_flux[np.searchsorted(self._bins_freq, freq),
-#                                np.searchsorted(self._bins_time, time)])
-#     def count(self, freq, time):
-#         return(self._bins_count[np.searchsorted(self._bins_freq, freq),
-#                                 np.searchsorted(self._bins_time, time)])
+        for index,value in enumerate(values):
+            values[index] = float(value)
+        del values[0]
+        del values[8]
+        values[5] = (values[5] - values[6])
+        values.insert(0, key)
+        dbc.execute(q_photon_add, values)
+        key = key + 1
+    db.commit()
 
+print("Input took {:.2f} seconds".format(time.clock()-start))
 
-# Input.Photons("test.delay_dump")            
+start = time.clock()
+Query(dbc, 0).equal("Resonance",416).in_range("Wavelength", 1475, 1625).run()
+print("Query took {:.2f} seconds".format(time.clock()-start))
+
+start = time.clock()
+tf_a = TransferFunction(dbc, [50,50], 0).in_range("Wavelength", 1450, 1650).run()
+print("TF took {:.2f} seconds".format(time.clock()-start))
+tf_a.plot()
+db.close()
